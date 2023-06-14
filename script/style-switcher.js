52e44617bfd97b4dad8858d4a7a8154215f74b81
Valentina-Assef <valenassef@gmail.com>
2023-06-14 13:10:14 -0300

    Theme Color

---
+++ script/style-switcher.js
@@ -0,0 +1,25 @@
+/* Toggle style switcher */
+let styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+/* Hide style - switcher on scroll */
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+/* Theme Color */
+let alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyle.forEach((style) => {
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
