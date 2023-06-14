fd34073c337d4100aec75fe27ddf55ca51f3038f
Valentina-Assef <valenassef@gmail.com>
2023-06-14 02:23:09 -0300

    Projects Section

---
+++ script/script.js
@@ -0,0 +1,18 @@
+/* Project Popup Section */
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("project-button")){
+        toggleProjectPopup();
+        projectItemDetails(e.target.parentElement);
+    }
+})
+
+function toggleProjectPopup(){
+    document.querySelector(".project-popup").classList.toggle("open");
+}
+
+document.querySelector(".project-popup-close").addEventListener("click", toggleProjectPopup)
+
+function projectItemDetails(projectItem){
+    document.querySelector(".pp-thumbnail img").src = projectItem.querySelector(".project-img").src;
+    document.querySelector(".project-popup-body").innerHTML = projectItem.querySelector(".project-item-details").innerHTML;
+}
